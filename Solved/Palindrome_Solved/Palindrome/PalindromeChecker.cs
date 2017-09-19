d4b2ac9a7c5f0c1ff305f63eee6a862346f45af5
Per Storgaard Laursen <perl@easj.dk>
2017-09-19 23:11:38 +0200

    Minor updates to a couple of projects
    Minor updates to a couple of projects

--- Solved/Palindrome_Solved/Palindrome/PalindromeChecker.cs
+++ Solved/Palindrome_Solved/Palindrome/PalindromeChecker.cs
@@ -12,13 +12,28 @@
 
         private bool IsPalindromeInternal(string phrase)
         {
+            // Trivial case
             if (phrase.Length < 2)
             {
                 return true;
             }
 
-            return phrase.Substring(0, 1) == phrase.Substring(phrase.Length - 1, 1) &&
-                   IsPalindromeInternal(phrase.Substring(1, phrase.Length - 2));
+            // Division :
+            //   Subproblem 1: Are the first and last letters equal to each other?
+            //   Subproblem 2: Is the remaining phrase (i.e. minus first and last letter)
+            //                 also a palindrome?
+            // Example: rotor
+
+            string firstLetter = phrase.Substring(0, 1); // = "r"
+            string lastLetter = phrase.Substring(phrase.Length - 1, 1); // = "r"
+            string remainingPhrase = phrase.Substring(1, phrase.Length - 2); // = "oto"
+
+            bool firstLastEqual = (firstLetter == lastLetter); // "r" == "r", so true
+            bool isRestPalindrome = IsPalindromeInternal(remainingPhrase); // called with "oto", so true
+
+            // Combination :
+            //   phrase is a palindrome if BOTH subproblems are true.
+            return firstLastEqual && isRestPalindrome;
         }
     }
 }
