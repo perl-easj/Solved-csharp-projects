be379c9a488b9f6cba378920cead7e876b17e80d
Per Storgaard Laursen <perl@easj.dk>
2017-10-09 22:17:55 +0200

    Updated solution to TestExampleC
    Updated solution to TestExampleC

--- Solved/TestExampleC_Solved/TestExampleC/CurrencyExchange.cs
+++ Solved/TestExampleC_Solved/TestExampleC/CurrencyExchange.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 
 namespace TestExampleC
 {
-    public class CurrencyExchange
+    public class CurrencyExchange : ICurrencyExchange
     {
         private Dictionary<string, double> _exchangeRates;
+        private ICurrencyCheck _currencyCheck;
 
-        public CurrencyExchange()
+        public CurrencyExchange(ICurrencyCheck currencyCheck)
         {
             _exchangeRates = new Dictionary<string, double>();
+            _currencyCheck = currencyCheck;
         }
 
         public void SetExchangeRate(string currencyCross, double exchangeRate)
         {
-            if (!IsLegalCurrencyCross(currencyCross))
+            if (!_currencyCheck.IsValidCurrencyCross(currencyCross))
             {
-                throw new ArgumentException(currencyCross + " is not a legal currency cross");
+                throw new ArgumentException(currencyCross + " is not a valid currency cross");
             }
 
             if (exchangeRate <= 0)
             {
-                throw new ArgumentException(exchangeRate + " is not a legal exchange rate");
+                throw new ArgumentException(exchangeRate + " is not a valid exchange rate");
             }
 
             _exchangeRates[currencyCross] = exchangeRate;
@@ -29,9 +31,9 @@ namespace TestExampleC
 
         public double DoExchange(string currencyCross, double amount)
         {
-            if (!IsLegalCurrencyCross(currencyCross))
+            if (!_currencyCheck.IsValidCurrencyCross(currencyCross))
             {
-                throw new ArgumentException(currencyCross + " is not a legal currency cross");
+                throw new ArgumentException(currencyCross + " is not a valid currency cross");
             }
 
             if (!_exchangeRates.ContainsKey(currencyCross))
@@ -41,25 +43,10 @@ namespace TestExampleC
 
             if (amount <= 0)
             {
-                throw new ArgumentException(amount + " is not a legal amount");
+                throw new ArgumentException(amount + " is not a valid amount");
             }
 
             return _exchangeRates[currencyCross] * amount;
-        }
-
-        private bool IsLegalCurrencyCross(string currencyCross)
-        {
-            // A currency cross must
-            // 1) Not be null, empty or only contains spaces
-            // 2) Be exactly six characters long
-            // 3) Be between two different currencies, i.e. USDUSD is not legal.
-            //
-            if (!string.IsNullOrWhiteSpace(currencyCross) && currencyCross.Length == 6)
-            {
-                return currencyCross.Substring(0,3) != currencyCross.Substring(3,3);
-            }
-
-            return false;
         }
     }
 }
--- Solved/TestExampleC_Solved/UnitTestProject/CurrencyExchangeTest.cs
+++ Solved/TestExampleC_Solved/UnitTestProject/CurrencyExchangeTest.cs
@@ -8,133 +8,146 @@ namespace UnitTestProject
     [TestClass]
     public class CurrencyExchangeTest
     {
+        private ICurrencyExchange _currencyExchange;
+
+        public CurrencyExchangeTest()
+        {
+            _currencyExchange = null;
+        }
+
+        private void Arrange()
+        {
+            _currencyExchange = new CurrencyExchange(new CurrencyCheck());
+        }
+
+        private void ArrangeWithStandardEntries()
+        {
+            Arrange();
+            _currencyExchange.SetExchangeRate("USDDKK", 6.50);
+            _currencyExchange.SetExchangeRate("EURDKK", 7.50);
+            _currencyExchange.SetExchangeRate("USDGBP", 0.80);
+        }
+
+
         [TestMethod]
         public void TestSetExchangeRate_InsertSingleLegal_OK()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act (& implicit Assert)
-            testSubject.SetExchangeRate("USDDKK", 6.50);
+            _currencyExchange.SetExchangeRate("USDDKK", 6.50);
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertSingleIllegal_TooShort_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { testSubject.SetExchangeRate("USDDK", 6.50); });
+            Assert.ThrowsException<ArgumentException>(() => { _currencyExchange.SetExchangeRate("USDDK", 6.50); });
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertSingleIllegal_TooLong_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { testSubject.SetExchangeRate("USDKroner", 6.50); });
+            Assert.ThrowsException<ArgumentException>(() => { _currencyExchange.SetExchangeRate("USDKroner", 6.50); });
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertSingleIllegal_SameCurrency_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { testSubject.SetExchangeRate("USDUSD", 6.50); });
+            Assert.ThrowsException<ArgumentException>(() => { _currencyExchange.SetExchangeRate("USDUSD", 6.50); });
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertSingleIllegal_Empty_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { testSubject.SetExchangeRate("      ", 6.50); });
+            Assert.ThrowsException<ArgumentException>(() => { _currencyExchange.SetExchangeRate("      ", 6.50); });
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertSingleIllegal_Null_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { testSubject.SetExchangeRate(null, 6.50); });
+            Assert.ThrowsException<ArgumentException>(() => { _currencyExchange.SetExchangeRate(null, 6.50); });
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertSingleIllegal_NegativeExchangeRate_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { testSubject.SetExchangeRate(null, -6.50); });
+            Assert.ThrowsException<ArgumentException>(() => { _currencyExchange.SetExchangeRate(null, -6.50); });
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertSingleIllegal_ZeroExchangeRate_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { testSubject.SetExchangeRate(null, 0.0); });
+            Assert.ThrowsException<ArgumentException>(() => { _currencyExchange.SetExchangeRate(null, 0.0); });
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertMultipleLegal_OK()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
-
-            // Act (& implicit Assert)
-            testSubject.SetExchangeRate("USDDKK", 6.50);
-            testSubject.SetExchangeRate("EURDKK", 7.50);
-            testSubject.SetExchangeRate("USDGBP", 0.80);
+            ArrangeWithStandardEntries();
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertMultipleWithIllegal_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act & Assert
-            testSubject.SetExchangeRate("USDDKK", 6.50);
-            Assert.ThrowsException<ArgumentException>(() => { testSubject.SetExchangeRate("EUDDK", 7.50); });
-            testSubject.SetExchangeRate("USDGBP", 0.80);
+            _currencyExchange.SetExchangeRate("USDDKK", 6.50);
+            Assert.ThrowsException<ArgumentException>(() => { _currencyExchange.SetExchangeRate("EUDDK", 7.50); });
+            _currencyExchange.SetExchangeRate("USDGBP", 0.80);
         }
 
         [TestMethod]
         public void TestSetExchangeRate_InsertAndUpdateSingleLegal_OK()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
+            Arrange();
 
             // Act (& implicit Assert)
-            testSubject.SetExchangeRate("USDDKK", 6.50);
-            testSubject.SetExchangeRate("USDDKK", 6.80);
+            _currencyExchange.SetExchangeRate("USDDKK", 6.50);
+            _currencyExchange.SetExchangeRate("USDDKK", 6.80);
         }
 
         [TestMethod]
         public void TestDoExchange_Existing_OK()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
-            testSubject.SetExchangeRate("USDDKK", 6.50);
-            testSubject.SetExchangeRate("EURDKK", 7.50);
-            testSubject.SetExchangeRate("USDGBP", 0.80);
+            ArrangeWithStandardEntries();
             double expectedResult = 1300.0;
 
             // Act
-            double actualResult = testSubject.DoExchange("USDDKK", 200.0);
+            double actualResult = _currencyExchange.DoExchange("USDDKK", 200.0);
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult, 0.0001);
@@ -144,15 +157,12 @@ namespace UnitTestProject
         public void TestDoExchange_ExistingWithUpdate_OK()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
-            testSubject.SetExchangeRate("USDDKK", 6.50);
-            testSubject.SetExchangeRate("EURDKK", 7.50);
-            testSubject.SetExchangeRate("USDGBP", 0.80);
-            testSubject.SetExchangeRate("USDDKK", 6.80);
+            ArrangeWithStandardEntries();
+            _currencyExchange.SetExchangeRate("USDDKK", 6.80);
             double expectedResult = 1360.0;
 
             // Act
-            double actualResult = testSubject.DoExchange("USDDKK", 200.0);
+            double actualResult = _currencyExchange.DoExchange("USDDKK", 200.0);
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult, 0.0001);
@@ -162,52 +172,40 @@ namespace UnitTestProject
         public void TestDoExchange_NonExisting_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
-            testSubject.SetExchangeRate("USDDKK", 6.50);
-            testSubject.SetExchangeRate("EURDKK", 7.50);
-            testSubject.SetExchangeRate("USDGBP", 0.80);
+            ArrangeWithStandardEntries();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { double actualResult = testSubject.DoExchange("USDNOR", 200.0); });
+            Assert.ThrowsException<ArgumentException>(() => { double actualResult = _currencyExchange.DoExchange("USDNOR", 200.0); });
         }
 
         [TestMethod]
         public void TestDoExchange_Illegal_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
-            testSubject.SetExchangeRate("USDDKK", 6.50);
-            testSubject.SetExchangeRate("EURDKK", 7.50);
-            testSubject.SetExchangeRate("USDGBP", 0.80);
+            ArrangeWithStandardEntries();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { double actualResult = testSubject.DoExchange("USDNK", 200.0); });
+            Assert.ThrowsException<ArgumentException>(() => { double actualResult = _currencyExchange.DoExchange("USDNK", 200.0); });
         }
 
         [TestMethod]
         public void TestDoExchange_Illegal_NegativeAmount_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
-            testSubject.SetExchangeRate("USDDKK", 6.50);
-            testSubject.SetExchangeRate("EURDKK", 7.50);
-            testSubject.SetExchangeRate("USDGBP", 0.80);
+            ArrangeWithStandardEntries();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { double actualResult = testSubject.DoExchange("USDDKK", -200); });
+            Assert.ThrowsException<ArgumentException>(() => { double actualResult = _currencyExchange.DoExchange("USDDKK", -200); });
         }
 
         [TestMethod]
         public void TestDoExchange_Illegal_ZeroAmount_Exception()
         {
             // Arrange
-            CurrencyExchange testSubject = new CurrencyExchange();
-            testSubject.SetExchangeRate("USDDKK", 6.50);
-            testSubject.SetExchangeRate("EURDKK", 7.50);
-            testSubject.SetExchangeRate("USDGBP", 0.80);
+            ArrangeWithStandardEntries();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => { double actualResult = testSubject.DoExchange("USDDKK", 0); });
+            Assert.ThrowsException<ArgumentException>(() => { double actualResult = _currencyExchange.DoExchange("USDDKK", 0); });
         }
     }
 }
