5712992dac07561d232226661f651c9acabff563
Per Storgaard Laursen <perl@easj.dk>
2018-01-30 20:43:13 +0100

    Added FunctionExample and GenericCatalog, plus updates to a few additional exercises.

--- Solved/WebShopV05_Solved/WebShopV05/InsertCodeHere.cs
+++ Solved/WebShopV05_Solved/WebShopV05/InsertCodeHere.cs
@@ -40,24 +40,6 @@ namespace WebShopV05
             Console.WriteLine($"You ordered {noOfBooksInOrder} books, {noOfDVDsInOrder} DVDs and {noOfGamesInOrder} games");
             Console.WriteLine($"Total cost including tax, shipping and credit card fee: {totalPrice} kr.");
 
-
-            // EXTRA (Step 4) - Calculation of discount
-            //
-            int totalNoOfItems = noOfDVDsInOrder + noOfBooksInOrder + noOfGamesInOrder;
-
-            // discount5 is 1 if totalNoOfItems is at least 15, otherwise 0
-            int discount5 = (totalNoOfItems / 15) - ((totalNoOfItems - 15) / 15);
-
-            // discount10 is 1 if totalNoOfItems is at least 30, otherwise 0
-            int discount10 = (totalNoOfItems / 30) - ((totalNoOfItems - 30) / 30);
-
-            // Calculate the total discount percentage
-            double discountPercentage = 5.0 * (discount5 + discount10);
-
-            // Uncomment the below lines to see price with discount
-            // totalPrice = totalPrice * (1 - 0.05*(discount5 + discount10));
-            // Console.WriteLine($"Total cost with discount: {totalPrice} kr.");
-
             // The LAST line of code should be ABOVE this line
         }
     }
